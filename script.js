b11a0b6cb64ae1a2242bf33f303c4ebb3296cb48
Augusto Zegarra <augusto.florszegarra@gmail.com>
2023-01-21 02:25:08 -0500

    Empezando encriptado

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const input = document.getElementById("input");
-    //...
- });
-
-const input = document.getElementById("input");
-const imagenBusqueda = document.getElementById("imagen-busqueda");
-const sinTexto = document.getElementById("sin-texto");
-const output = document.createElement("textarea");
-
-input.addEventListener("input", function() {
-  if (this.value.length > 0) {
-    imagenBusqueda.style.display = "none";
-    sinTexto.style.display = "none";
-    output.style.display = "block";
-    output.setAttribute("id","output")
-    document.getElementsByClassName("main-right")[0].appendChild(output);
-  } else {
-    imagenBusqueda.style.display = "block";
-    sinTexto.style.display = "block";
-    output.style.display = "none";
-    output.remove();
-  }
-});
+const textArea = document.querySelector(".text-area");
+console.log(textArea.value);
+const btnEncriptar = document.querySelector("#btn-encriptar");
+const btnEncriptar2 = document.getElementById("btn-encriptar");
